5025d21e14c5f1ff780b96fd97a151e07a703fe1
@j_kahuha <johntexavir@gmail.com>
2022-08-14 00:09:04 +0300

    add env variables

--- client/src/utils/Geolocation/geocoding.js
+++ client/src/utils/Geolocation/geocoding.js
@@ -1,28 +1,23 @@
 import { makeGETrequest } from '../../context';
-const REACT_APP_OPENCAGE_API_KEY = process.env.REACT_APP_OPENCAGE_API_KEY;
+
+const base_uri = process.env.REACT_APP_OPENCAGE_URI;
+const api_key = process.env.REACT_APP_OPENCAGE_API_KEY;
 
 export const geocoder = async (address, dispatch) => {
-  //Physical Address Geocoding to Geometric transformations
-
-  //use opencageData Googlemaps Api requesting Billing address
-  const base_uri = 'https://api.opencagedata.com/geocode/v1/json';
-  console.log('SS', REACT_APP_OPENCAGE_API_KEY);
-  const uri = `${base_uri}?q=${address}&key=${REACT_APP_OPENCAGE_API_KEY}`;
+  const uri = `${base_uri}?q=${address}&key=${api_key}`;
 
   return await makeGETrequest(uri, dispatch, true);
 };
 
 export async function reverseGeocode(dispatch, coords) {
   dispatch({ type: 'REQUEST_API' });
-  console.log('KK', process.env.REACT_APP_OPENCAGE_API_KEY);
   var query = coords[0] + ',' + coords[1];
-  var api_url = 'https://api.opencagedata.com/geocode/v1/json';
 
   var request_url =
-    api_url +
+    base_uri +
     '?' +
     'key=' +
-    REACT_APP_OPENCAGE_API_KEY +
+    api_key +
     '&q=' +
     encodeURIComponent(query) +
     '&pretty=1';
